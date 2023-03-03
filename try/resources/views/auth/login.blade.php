77343f2afee45fecbe801bdfc55bbf65e9c29c1c
Kristupas <114056612+Kplukas@users.noreply.github.com>
2023-03-03 18:00:11 +0200

    Home, welcome, login, register BASIC style

--- try/resources/views/auth/login.blade.php
+++ try/resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card  text-white bg-secondary border border-5 border-white">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -18,9 +18,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +32,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -53,14 +53,14 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-dark btn-outline-light">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn link-light" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
--- try/resources/views/welcome.blade.php
+++ try/resources/views/welcome.blade.php
@@ -4,23 +4,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card text-white bg-secondary border border-5 border-white">
                 <div class="card-body" style="min-height: 70vh;">
-                    <h2>Welcome to BEAUTY FOR YOU!</h2>
-                    <p>Select your beauty services at our web platform.</p>
-                    <p>New salons, beauty experts and services are being added often!</p>
-                    <h4>We are missing something? Please let us know and we will add it in no time!</h4>
+                    <h2 class="text-center m-2 border-bottom border-white border-3">Welcome to BEAUTY FOR YOU!</h2>
+                    <p class="text-center mt-5">Select your beauty services at our web platform.</p>
+                    <p class="text-center mt-2">New salons, beauty experts and services are being added often!</p>
                     @guest
-                    <h3>
-                        <a href="{{ route('login') }}">Log in</a> or <a href="{{ route('register') }}">Sign up</a> to get started!
+                    <h3 class="mt-5 p-5 text-center border-top border-bottom border-white border-3">
+                        <a class="btn btn-dark btn-outline-light col-3 fs-2" href="{{ route('login') }}">Log in</a>
+                        or
+                        <a class="btn btn-dark btn-outline-light col-3 fs-2" href="{{ route('register') }}">Sign up</a>
+                        to get started!
                     </h3>
                     @else
-                    <h3>Browse beauty:
-                        <a href="{{route('salon-index2')}}">Salons </a>
-                        <a href="{{route('master-index2')}}">Experts </a>
-                        <a href="{{route('service-index2')}}">Services </a>
+                    <h3 class="mt-5 p-5 text-center border-top border-bottom border-white border-3">Browse beauty: <br>
+                        <a class="btn btn-dark btn-outline-light col-3 m-2" href="{{route('salon-index2')}}">Salons </a>
+                        <a class="btn btn-dark btn-outline-light col-3 m-2" href="{{route('master-index2')}}">Experts </a>
+                        <a class="btn btn-dark btn-outline-light col-3 m-2" href="{{route('service-index2')}}">Services </a>
                     </h3>
                     @endguest
+                    <h4 class="text-center mt-5">We are missing something? Please let us know and we will add it in no time!</h4>
                 </div>
             </div>
         </div>
